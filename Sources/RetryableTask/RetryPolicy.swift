f644eb35e0f2df9900b5f1dccef445ff737e60db
Gustaf Rosenblad <gustaf@ccode.se>
2022-08-16 21:47:35 +0200

    feat: Added RetryPolicy-protocol

---
+++ Sources/RetryableTask/RetryPolicy.swift
@@ -0,0 +1,16 @@
+/// A policiy to be used along with `withRetryableTask()`. Defines logic for when retry should be made.
+///
+/// Built-in policies are:
+/// - `DefaultRetryPolicy`
+/// - `ImmediateRetryPolicy`
+/// - `DelayedRetryPolicy`
+/// - `ExponentialBackoffDelayRetryPolicy`
+/// - `NoRetryPolicy`
+public protocol RetryPolicy {
+
+    /// `true` if a retry should be made, else `false`. Called before every execution except the first one.
+    var shouldRetry: Bool { get }
+
+    /// Informs the policy that a retry will be made. Called before every exeyction except the first one.
+    mutating func beforeRetry() async throws
+}
